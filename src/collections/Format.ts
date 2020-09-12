5c28cdc4e4c04153734c445718fb7d5a1de9ace1
Jaslyn S <7008316+aelesia@users.noreply.github.com>
2020-09-13 01:58:15 +0800

    Format.TimeAgo

--- src/collections/Format.ts
+++ src/collections/Format.ts
@@ -7,4 +7,20 @@ export class Format {
     console.warn(new TypeError(`Unable to parse value as string: ${value}`))
     return 'ERR'
   }
+  static timeAgo(ms: number): string
+  static timeAgo(ms: unknown): string {
+    if (typeof ms === 'number') {
+      if (ms < 60000) {
+        return `${Math.floor(ms / 1000)} seconds`
+      } else if (ms < 3600000) {
+        return `${Math.floor(ms / 60000)} minutes`
+      } else if (ms < 86400000) {
+        return `${Math.floor(ms / 3600000)} hours`
+      } else {
+        return `${Math.floor(ms / 86400000)} days`
+      }
+    }
+    console.warn(new TypeError(`Unable to parse value as number: ${ms}`))
+    return 'ERR'
+  }
 }
--- tests/misc/Format.test.ts
+++ tests/misc/Format.test.ts
@@ -1,4 +1,5 @@
 import { _ } from '../../index'
+import { TimeUtil } from '../../src/collections/util/TimeUtil'
 
 describe('Format', () => {
   let consoleWarn: any
@@ -12,6 +13,36 @@ describe('Format', () => {
     })
     test('Invalid Email', () => {
       expect(_.format.email(123 as any)).toEqual('ERR')
+      expect(consoleWarn).toBeCalled()
+    })
+  })
+
+  describe('timeAgo', () => {
+    test('seconds', () => {
+      expect(_.format.timeAgo(0)).toEqual('0 seconds')
+      expect(_.format.timeAgo(TimeUtil.secs(1))).toEqual('1 seconds')
+      expect(_.format.timeAgo(TimeUtil.secs(59))).toEqual('59 seconds')
+      expect(_.format.timeAgo(TimeUtil.secs(60))).not.toEqual('59 seconds')
+    })
+    test('minutes', () => {
+      expect(_.format.timeAgo(TimeUtil.secs(60))).toEqual('1 minutes')
+      expect(_.format.timeAgo(TimeUtil.mins(1))).toEqual('1 minutes')
+      expect(_.format.timeAgo(TimeUtil.mins(59))).toEqual('59 minutes')
+      expect(_.format.timeAgo(TimeUtil.mins(60))).not.toEqual('60 minutes')
+    })
+    test('hours', () => {
+      expect(_.format.timeAgo(TimeUtil.mins(60))).toEqual('1 hours')
+      expect(_.format.timeAgo(TimeUtil.hours(1))).toEqual('1 hours')
+      expect(_.format.timeAgo(TimeUtil.hours(23))).toEqual('23 hours')
+      expect(_.format.timeAgo(TimeUtil.hours(24))).not.toEqual('24 hours')
+    })
+    test('days', () => {
+      expect(_.format.timeAgo(TimeUtil.hours(24))).toEqual('1 days')
+      expect(_.format.timeAgo(TimeUtil.days(1))).toEqual('1 days')
+      expect(_.format.timeAgo(TimeUtil.days(30))).toEqual('30 days')
+    })
+    test('invalid', () => {
+      expect(_.format.timeAgo('abc' as any)).toEqual('ERR')
       expect(consoleWarn).toBeCalled()
     })
   })
